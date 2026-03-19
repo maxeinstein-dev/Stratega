b9505588486df30cf79eedf1153265b341bef5c0
Maxsuel Einstein <maxsueleinstein.dev@gmail.com>
2026-03-19 18:47:15 -0300

    Segunda parte do commit inicial

---
+++ src/main/java/br/com/maxsueleinstein/stratega/StrategaApplication.java
@@ -0,0 +1,13 @@
+package br.com.maxsueleinstein.stratega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrategaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/maxsueleinstein/stratega/StrategaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maxsueleinstein.stratega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
