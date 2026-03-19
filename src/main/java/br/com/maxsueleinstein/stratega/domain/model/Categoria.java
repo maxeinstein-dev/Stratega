d4f6d020b2b8b4413c7613f7c1bfa7e652a6fdd7
Maxsuel Einstein <maxsueleinstein.dev@gmail.com>
2026-03-19 18:47:35 -0300

    Adiciona modelos

---
+++ src/main/java/br/com/maxsueleinstein/stratega/domain/model/Categoria.java
@@ -0,0 +1,37 @@
+package br.com.maxsueleinstein.stratega.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+
+    public Categoria (){}
+
+    public Categoria(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
