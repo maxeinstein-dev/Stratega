af0522cc358e8ffce7bfb033c538f48e68d6d6e2
Maxsuel Einstein <maxsueleinstein.dev@gmail.com>
2026-03-26 22:21:04 -0300

    feat:add service methods

--- src/main/java/br/com/maxsueleinstein/stratega/service/UserService.java
+++ src/main/java/br/com/maxsueleinstein/stratega/service/UserService.java
@@ -1,8 +1,32 @@
 package br.com.maxsueleinstein.stratega.service;
 
+import br.com.maxsueleinstein.stratega.domain.dto.CreateUserDTO;
+import br.com.maxsueleinstein.stratega.domain.dto.UserResponseDTO;
+import br.com.maxsueleinstein.stratega.domain.entity.User;
+import br.com.maxsueleinstein.stratega.mapper.UserMapper;
+import br.com.maxsueleinstein.stratega.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public UserResponseDTO createUser(CreateUserDTO dto) {
+        String encodedPassword = passwordEncoder.encode(dto.password());
+
+        User user = UserMapper.toEntity(dto, encodedPassword);
+
+        User saved = userRepository.save(user);
+
+        return UserMapper.toResponse(saved);
+    }
 }
--- src/main/java/br/com/maxsueleinstein/stratega/service/WalletService.java
+++ src/main/java/br/com/maxsueleinstein/stratega/service/WalletService.java
@@ -1,8 +1,30 @@
 package br.com.maxsueleinstein.stratega.service;
 
+import br.com.maxsueleinstein.stratega.domain.entity.User;
+import br.com.maxsueleinstein.stratega.domain.entity.Wallet;
+import br.com.maxsueleinstein.stratega.repository.UserRepository;
+import br.com.maxsueleinstein.stratega.repository.WalletRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WalletService {
+    private final WalletRepository walletRepository;
+    private final UserRepository userRepository;
 
+    public WalletService(WalletRepository walletRepository, UserRepository userRepository) {
+        this.walletRepository = walletRepository;
+        this.userRepository = userRepository;
+    }
+
+
+    public Wallet createWallet(Wallet wallet) {
+
+        Long userId = wallet.getUser().getId();
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        wallet.setUser(user);
+        return walletRepository.save(wallet);
+    }
 }
