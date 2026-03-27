9c9176b5ff4a87cf88c957b4c21a804fdb547805
Maxsuel Einstein <maxsueleinstein.dev@gmail.com>
2026-03-26 22:21:40 -0300

    feat:add method

--- src/main/java/br/com/maxsueleinstein/stratega/repository/UserRepository.java
+++ src/main/java/br/com/maxsueleinstein/stratega/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package br.com.maxsueleinstein.stratega.repository;
 
 import br.com.maxsueleinstein.stratega.domain.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(@Email @NotNull String email);
 }
