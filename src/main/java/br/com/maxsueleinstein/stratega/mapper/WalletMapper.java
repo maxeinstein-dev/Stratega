6f01811ae53c4d5b618901476dbc81c2bd483487
Maxsuel Einstein <maxsueleinstein.dev@gmail.com>
2026-03-30 21:30:11 -0300

    feat: add wallet mapper

--- src/main/java/br/com/maxsueleinstein/stratega/mapper/WalletMapper.java
+++ src/main/java/br/com/maxsueleinstein/stratega/mapper/WalletMapper.java
@@ -1,4 +1,20 @@
 package br.com.maxsueleinstein.stratega.mapper;
 
+import br.com.maxsueleinstein.stratega.domain.dto.WalletResponseDTO;
+import br.com.maxsueleinstein.stratega.domain.entity.User;
+import br.com.maxsueleinstein.stratega.domain.entity.Wallet;
+
 public class WalletMapper {
+
+    public static Wallet toEntity(WalletResponseDTO dto, User user) {
+        return new Wallet(
+                dto.name(), dto.type(), dto.balance(), user
+        );
+    }
+
+    public static WalletResponseDTO toResponse(Wallet wallet) {
+        return new WalletResponseDTO(
+                wallet.getId(), wallet.getName(), wallet.getType(), wallet.getBalance()
+        );
+    }
 }
