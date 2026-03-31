584a35b8959c3ffaa43263b469204dfa5addd073
Maxsuel Einstein <maxsueleinstein.dev@gmail.com>
2026-03-30 21:31:53 -0300

    feat: create findByUserId for Wallet

--- src/main/java/br/com/maxsueleinstein/stratega/repository/WalletRepository.java
+++ src/main/java/br/com/maxsueleinstein/stratega/repository/WalletRepository.java
@@ -4,6 +4,9 @@ import br.com.maxsueleinstein.stratega.domain.entity.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
+    List<Wallet> findByUserId(Long userId);
 }
