9ae44791316cd27aa201501ae2541cc4093905b1
Maxsuel Einstein <maxsueleinstein.dev@gmail.com>
2026-04-01 03:41:54 -0300

    fix: transactions DTOs

--- src/main/java/br/com/maxsueleinstein/stratega/domain/dto/TransactionRequestDTO.java
+++ src/main/java/br/com/maxsueleinstein/stratega/domain/dto/TransactionRequestDTO.java
@@ -1,4 +1,17 @@
 package br.com.maxsueleinstein.stratega.domain.dto;
 
-public record TransactionRequestDTO() {
+import br.com.maxsueleinstein.stratega.domain.entity.TransactionType;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record TransactionRequestDTO(Long userId,
+                                    String description,
+                                    BigDecimal amount,
+                                    LocalDateTime date,
+                                    TransactionType type,
+                                    Long walletId,
+                                    Long destinationWalletId,
+                                    String categoryName
+) {
 }
--- src/main/java/br/com/maxsueleinstein/stratega/domain/dto/TransactionResponseDTO.java
+++ src/main/java/br/com/maxsueleinstein/stratega/domain/dto/TransactionResponseDTO.java
@@ -3,8 +3,7 @@ package br.com.maxsueleinstein.stratega.domain.dto;
 import br.com.maxsueleinstein.stratega.domain.entity.TransactionType;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
-public record TransactionResponseDTO(Long id, String description, BigDecimal amount, LocalDateTime date,
-                                     TransactionType type, String categoryName) {
+public record TransactionResponseDTO(Long id, String description, BigDecimal amount,
+                                     TransactionType type, BigDecimal walletBalance) {
 }
